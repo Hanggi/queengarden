473d9a3d56f44b597a60ab9046b0ae950ec31228
Hanggi <>
2024-03-22 04:32:14 +0900

    test global.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Header from "@/Components/Shared/Header";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
